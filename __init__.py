fd71e45bc7afcb75cc9aaa623acc535bf4ef41a9
Ubuntu <ubuntu@ip-172-31-38-216.us-east-2.compute.internal>
2018-07-26 20:08:54 +0000

    Added some Assets for updates

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from CTFd.utils import admins_only, is_admin
 from CTFd.models import db, Challenges, Keys, Awards, Solves, Files, Tags, Teams
 from CTFd import utils
 from logging import getLogger, basicConfig,DEBUG,ERROR
-from CTFd.plugins import challenges
+from CTFd.plugins import challenges, register_plugin_assets_directory
 from CTFd import utils, CTFdFlask
 from flask import session
 
@@ -31,7 +31,19 @@ class SmartCityChallenge(Challenges):
 class SmartCity(challenges.BaseChallenge):
 	id = "smart_city"
 	name = "smart_city"
+        
+
+	templates = { # Handlebars template used for each aspect of challenge editing and viewing
+                        'create' : '/plugins/CTFd_SmartCity/assets/smartcity-challenge-create.njk',
+        		'update' : '/plugins/CTFd_SmartCity/assets/smartcity-challenge-update.njk'
+	}
+
+        scripts = {
+                        'create' : '/plugins/CTFd_SmartCity/assets/smartcity-challenge-create.js',
+			'update' : '/plugins/CTFd_SmartCity/assets/smartcity-challenge-update.js'
+        }
 	
+	@staticmethod
 	def create(request):
 		"""
 		This method is used to process the challege creation request.
@@ -39,6 +51,7 @@ class SmartCity(challenges.BaseChallenge):
 		:param request:
 		:return:
 		"""
+	
 		chal = SmartCityChallenge(
 			name= request.form['name'],
 			description = request.form['description'],
@@ -61,14 +74,42 @@ class SmartCity(challenges.BaseChallenge):
 		logger.debug("Genereted buildingId " + chal.buildingId + " for challenge " + chal.name)
 		
 		db.session.add(chal)
-		db.session.commit()  
+		db.session.commit() 
+
+	
+	@staticmethod
+	def read(challenge):
 		
+		challenge = SmartCityChallenge.query.filter_by(id=challenge.id).first()	 
+		data = {
+			'id': challenge.id,
+			'name': challenge.name,
+			'value': challenge.value,
+			'description': challenge.description,
+			'category': challenge.category,
+			'hidden': challenege.hidden,
+			'max_attempts': challenge.max_attempts,
+			'buildingId': challenge.buildingId,
+			'type': challenge.type,
+			'type_data': {
+				'id': SmartCity.id,
+				'name': SmartCity.name,
+				'templates': SmartCity.templates,
+				'scripts': SmartCity.scripts,
+			}
+		
+		}
+		return challenge, data
 
 def load(app):
     """load overrides for smart_city to work properly"""
     logger.setLevel(app.logger.getEffectiveLevel())
     app.db.create_all()
+    register_plugin_assets_directory(app, base_path='/plugins/CTFd_SmartCity/assets')
+    challenges.CHALLENGE_CLASSES['smart_city'] = SmartCity   
+     
     
+
     #challenges.CHALLENGE_CLASSES["smart_city"] = SmartCity
     #def view_challenges():
         #return render_template('page.html', content="<h1>Challenges are currently closed</h1>")
